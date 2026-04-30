de3c3c03e3c75cfcc762743c51d7e1037a2baf22
kyul130528-hash <kyul130528@gmail.com>
2026-04-30 21:19:04 +0900

    Update python main.py

--- python main.py
+++ python main.py
@@ -1,1 +1,22 @@
+import discord
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'로그인 완료: {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "!뽑기":
+        await message.channel.send("🎴 카드 뽑기 성공!")
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+client.run(e80dZ8ojjkzNV_9k2GpSK-sH-s_KeXb-)
